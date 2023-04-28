717841333eeb2b1f2efffef52e5889bd39725629
hrexp <none>
2023-04-27 23:31:05 -0600

    added code to use console or slack for messaging

--- load.py
+++ load.py
@@ -4,9 +4,24 @@ import os
 from dotenv import load_dotenv
 import openai 
 from dataclasses import dataclass, field
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
+import ssl
+import certifi
+import argparse
+
+load_dotenv()
+
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+
+parser = argparse.ArgumentParser(description='NetGPT applicatoin')
+parser.add_argument('--mode', choices=['slack', 'cli'], default='cli',
+                    help='Choose the mode of operation (default: cli)')
+args = parser.parse_args()
 
 keys_file = str(pathlib.Path(__file__).resolve().parents[1])+"/keys.json"
-load_dotenv()
 
 @dataclass
 class Conversation:
@@ -25,14 +40,34 @@ def openai_message(message: str, conv: Conversation) -> Conversation:
     return conv 
 
 
-
-message_log = []
-conv = Conversation()
-while(True):
-    message = input('netops> ')
-    openai_message(message, conv)
-    print(conv.message_log[-1:])
-
-    
+def cli_mode():
+    message_log = []
+    conv = Conversation()
+    while(True):
+        message = input('netops> ')
+        openai_message(message, conv)
+        print(conv.message_log[-1]['content'])
 
 
+def slack_mode():
+    SLACK_BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    SLACK_APP_TOKEN = os.environ.get('SOCKET_TOKEN')
+    app = App(client=WebClient(token=SLACK_BOT_TOKEN,ssl=ssl_context))
+    conv = Conversation()
+
+    @app.event(event="message")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    def handle_message(body, say):
+        message = body['event']['text']
+        test = openai_message(message, conv)
+        say(test.message_log[-1]['content'])
+
+    handler = SocketModeHandler(app, app_token= SLACK_APP_TOKEN)
+    handler.start()
+
+if args.mode == 'cli':
+    cli_mode()
+if args.mode == 'slack':
+    slack_mode()
+
+
+        
